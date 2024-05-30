67f903f1e9aee82ee0160409be10cfb377a8789a
solfilius <68471507+SolRaze@users.noreply.github.com>
2024-05-30 15:15:16 +0530

    Create misc_commands.py

---
+++ cogs/misc_commands.py
@@ -0,0 +1,21 @@
+# Imports
+
+from discord.ext import commands
+
+
+#
+
+class misc_command(commands.Cog):
+  def __init__(self, bot):
+    self.bot = bot
+
+  @commands.command(name='repeat', aliases=['r', 'rep'])
+
+  async def repeat_command(self, ctx, *, arg):
+    await ctx.send(arg)
+
+
+
+
+def setup(bot):
+  bot.add_cog(misc_command(bot))
